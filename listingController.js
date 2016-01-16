da263ac266e6594ecead83fd6ce33c267e4ed4dc
Killian McCoy <buzzkillian@ufl.edu>
2016-01-15 21:17:44 -0500

    Fully functional app

--- listingController.js
+++ listingController.js
@@ -1,14 +1,55 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.newListing = {
+      name: '',
+      code: '',
+      coordinates: {
+        latitude: '',
+        longitude: ''
+      }
+    };
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.clearFields = function() {
+      $scope.newListing.code = '';
+      $scope.newListing.name = '';
+      $scope.newListing.latitude = '';
+      $scope.newListing.longitude = '';
+      $scope.newListing.address = '';
+    }
+
+    $scope.addListing = function() {
+
+      if ($scope.newListing.code != '' || $scope.newListing.name != '') {
+        var toInsert = {
+          name: $scope.newListing.name,
+          code: $scope.newListing.code,
+          coordinates: {
+            latitude: $scope.newListing.coordinates.latitude,
+            longitude: $scope.newListing.coordinates.longitude
+          },
+          address: $scope.newListing.address
+        };
+
+        $scope.listings.push(toInsert);
+      }
+
+    };
+
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+      $scope.detailedInfo = undefined;
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
